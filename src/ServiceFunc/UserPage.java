6000d0864eb111b753fb420bad50b2ca173024cf
YerinCho <forjustice9839@gmail.com>
2020-12-01 02:53:46 +0900

    refactor: refactor userProfile method  change switch to if statement  create User class

--- src/ServiceFunc/UserPage.java
+++ src/ServiceFunc/UserPage.java
@@ -26,7 +26,7 @@ public class UserPage {
             inputUserPageMenu(con, userInfo);
             System.out.println("See you later, " + userInfo.get(0) + "!");
         } catch (SQLException e) {
-            e.getStackTrace();
+            e.printStackTrace();
         }
     }
 
@@ -194,142 +194,133 @@ public class UserPage {
         }
     }
 
-    private static void userProfile(Connection con, ArrayList<String> userInfo) {
-        HashMap<String, String> profile = new LinkedHashMap<>();
-        Vector<String> vec = new Vector<>();
-        vec.add(0, "SSN");
-        vec.add(1, "name");
-        vec.add(2, "age");
-        vec.add(3, "email");
-        vec.add(4, "ID");
-        vec.add(5, "PW");
-        vec.add(6, "Nickname");
+    private static void userProfile(Connection con, ArrayList<String> userInfo) throws SQLException {
+        ResultSet rs = DatabaseQuery.findProfile(con, userInfo.get(1));
+        User user = getUser(rs);
 
-        try {
-            ResultSet rs = DatabaseQuery.findProfile(con, userInfo.get(1));
-            while (rs.next()) {
+        String curSsn = "'" + user.getSSN() + "'";
 
-                for (int i = 0; i < vec.size(); i++) {
-                    if (i != 2) {
-                        profile.put(vec.get(i), rs.getString(vec.get(i)));
-                    } else
-                        profile.put(vec.get(i), String.valueOf(rs.getInt(vec.get(i))));
-                }
-            }
-            rs.close();
+        int instr = -1;
+        while (instr == -1) {
+            System.out.print(
+                "\nPROFILE OPTIONS:\nPress 1 to see my profile.\nPress 2 to change personal information(name,age)."
+                    +
+                    "\nPress 3 tp change email address." +
+                    "\nPress 4 to change Password.\nPress 5 to change Nickname.\nPress 0 to return.\nYour Choice: ");
 
-            String curSsn = "'" + profile.get("SSN") + "'";
+            instr = parse(sc.nextLine());
 
-            Character instr = '\0';
-            while (!instr.equals('0')) {
-
-                System.out.print(
-                    "\nPROFILE OPTIONS:\nPress 1 to see my profile.\nPress 2 to change personal information(name,age)."
-                        +
-                        "\nPress 3 tp change email address." +
-                        "\nPress 4 to change Password.\nPress 5 to change Nickname.\nPress 0 to return.\nYour Choice: ");
-
-                instr = sc.nextLine().charAt(0);
-
-                switch (instr) {
-                    case '0' -> {
-                        System.out.println("Return to user page...");
-                    }
-
-                    case '1' -> {
-                        System.out.println("\n- PROFILE -");
-                        profile.forEach((key, value) -> System.out.println(key + ": " + value));
-                        instr = '0';
-                    }
-
-                    case '2' -> {
-                        String newName = null;
-                        int newAge = 0;
-                        System.out.print("Your new name: ");
-                        newName = sc.nextLine();
-                        System.out.print("Your new age: ");
-                        newAge = Integer.parseInt(sc.nextLine());
-
-                        DatabaseQuery.updateProfile(con, DatabaseHandler.USER, "name", "'" + newName + "'",
-                            "SSN = " + curSsn);
-                        DatabaseQuery.updateProfile(con, DatabaseHandler.USER, "age", newAge, "SSN = " + curSsn);
-
-                        System.out.println("Change Applied!\n");
-                        instr = '0';
-                    }
-
-                    case '3' -> {
-                        String newEmail = null;
-                        System.out.print("Your new Email: ");
-                        newEmail = sc.nextLine();
-
-                        ResultSet checkedResult = GeneralQuery.generalCheck(con, "email", DatabaseHandler.USER,
-                            "email = '" + newEmail + "'");
-
-                        if (!checkedResult.next()) {
-                            DatabaseQuery.updateProfile(con, DatabaseHandler.USER, "email", "'" + newEmail + "'",
-                                "SSN = " + curSsn);
-                            System.out.println("\nChange Applied!\n");
-                        } else {
-                            System.out.println(
-                                "\nRequest Denied: The email " + newEmail + " is currently used by other user\n");
-                        }
-                        instr = '0';
-                    }
-
-                    case '4' -> {
-                        String oldPassword = null;
-                        String newPassword = null;
-
-                        System.out.print("Your current password: ");
-                        oldPassword = sc.nextLine();
-
-                        if (!(oldPassword.equals(profile.get("PW"))))
-                            System.out.println("Request Denied: Didn't match with original password\n");
-                        else {
-                            String same = null;
-                            System.out.print("Your new password: ");
-                            newPassword = sc.nextLine();
-                            System.out.print("Your new password (confirm): ");
-                            same = sc.nextLine();
-
-                            if (newPassword.equals(same)) {
-                                DatabaseQuery.updateProfile(con, DatabaseHandler.ACCOUNT, "PW", "'" + newPassword + "'",
-                                    "Ussn = " + curSsn);
-                                System.out.println("\nChange Applied!\n");
-                            } else {
-                                System.out.println("ERROR: New password didn't match.\n");
-                            }
-                        }
-
-                        instr = '0';
-                    }
-
-                    case '5' -> {
-                        String newNickName = null;
-                        Character answer = '\0';
-                        System.out.print("Your new Nickname: ");
-                        newNickName = sc.nextLine();
-
-                        System.out.print(
-                            "Your new Nickname is " + newNickName + ". Am I right? (Enter y to say Yes): ");
-                        answer = sc.nextLine().charAt(0);
-                        if (answer.equals('y') || answer.equals('Y')) {
-                            DatabaseQuery.updateProfile(con, DatabaseHandler.ACCOUNT, "Nickname",
-                                "'" + newNickName + "'", "Ussn = " + curSsn);
-                            System.out.println("\nChange Applied!\n");
-                        } else {
-                            System.out.println("Cancel the changes...");
-                        }
-                        instr = '0';
-                    }
-                }
+            if (instr == 0) {
+                System.out.println("Return to user page...");
             }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+            if (instr == 1) {
+                printProfile(user);
+            }
+
+            if (instr == 2) {
+                updateNameAndAge(con, curSsn);
+            }
+
+            if (instr == 3) {
+                updateEmail(con, curSsn);
+            }
+
+            if (instr == 4) {
+                updatePassword(con, curSsn, user);
+            }
+
+            if (instr == 5) {
+                updateNickName(con, curSsn);
+            }
+        }
+    }
+
+    private static void updateNickName(Connection con, String curSsn) {
+        System.out.print("Your new Nickname: ");
+        String newNickName = sc.nextLine();
+
+        System.out.print(
+            "Your new Nickname is " + newNickName + ". Am I right? (Enter y to say Yes): ");
+        Character answer = sc.nextLine().charAt(0);
+
+        if (answer.equals('y') || answer.equals('Y')) {
+            DatabaseQuery.updateProfile(con, DatabaseHandler.ACCOUNT, "Nickname",
+                "'" + newNickName + "'", "Ussn = " + curSsn);
+            System.out.println("\nChange Applied!\n");
+            return;
         }
 
+        System.out.println("Cancel the changes...");
+    }
+
+    private static void updatePassword(Connection con, String curSsn, User user) {
+        System.out.print("Your current password: ");
+        String oldPassword = sc.nextLine();
+
+        if (!oldPassword.equals(user.getPassword())) {
+            System.out.println("Request Denied: Didn't match with original password\n");
+            return;
+        }
+
+        System.out.print("Your new password: ");
+        String newPassword = sc.nextLine();
+        System.out.print("Your new password (confirm): ");
+        String matchNewPassword = sc.nextLine();
+
+        if (newPassword.equals(matchNewPassword)) {
+            DatabaseQuery.updateProfile(con, DatabaseHandler.ACCOUNT, "PW", "'" + newPassword + "'",
+                "Ussn = " + curSsn);
+            System.out.println("\nChange Applied!\n");
+            return;
+        }
+
+        System.out.println("ERROR: New password didn't match.\n");
+    }
+
+    private static void updateEmail(Connection con, String curSsn) throws SQLException {
+        System.out.print("Your new Email: ");
+        String newEmail = sc.nextLine();
+
+        ResultSet checkedResult = GeneralQuery.generalCheck(con, "email", DatabaseHandler.USER,
+            "email = '" + newEmail + "'");
+
+        if (checkedResult.next()) {
+            System.out.println(
+                "\nRequest Denied: The email " + newEmail + " is currently used by other user\n");
+            return;
+        }
+        DatabaseQuery.updateProfile(con, DatabaseHandler.USER, "email", "'" + newEmail + "'",
+            "SSN = " + curSsn);
+        System.out.println("\nChange Applied!\n");
+    }
+
+    private static void updateNameAndAge(Connection con, String curSsn) {
+        System.out.print("Your new name: ");
+        String newName = sc.nextLine();
+        System.out.print("Your new age: ");
+        int newAge = Integer.parseInt(sc.nextLine());
+
+        DatabaseQuery.updateProfile(con, DatabaseHandler.USER, "name", "'" + newName + "'",
+            "SSN = " + curSsn);
+        DatabaseQuery.updateProfile(con, DatabaseHandler.USER, "age", newAge, "SSN = " + curSsn);
+
+        System.out.println("Change Applied!\n");
+    }
+
+    private static void printProfile(User user) {
+        System.out.println("\n- PROFILE -");
+        System.out.println(user.toString());
+    }
+
+    private static User getUser(ResultSet rs) throws
+        SQLException {
+        User user = null;
+        if (rs.next()) {
+            user = new User(rs);
+        }
+        rs.close();
+        return user;
     }
 
     private static void printMusicAttributes(ResultSet queryResult, List<MusicAttribute> musicAttributes) throws
@@ -382,7 +373,7 @@ public class UserPage {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            System.out.println("숫자만 입력해주세요.");
+            System.out.println("Input Error!: Press Only Number.");
         }
         return -1;
     }
